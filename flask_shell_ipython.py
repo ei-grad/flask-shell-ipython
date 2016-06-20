554ff749e5104d0f2a32d7cb8c37f99a0f4a2bb2
Andrew Grigorev <andrew@ei-grad.ru>
2016-06-20 18:46:56 +0300

    Add IPython version to banner

--- flask_shell_ipython.py
+++ flask_shell_ipython.py
@@ -18,9 +18,10 @@ def shell():
     import IPython
     from flask.globals import _app_ctx_stack
     app = _app_ctx_stack.top.app
-    banner = 'Python %s on %s\nApp: %s%s\nInstance: %s' % (
+    banner = 'Python %s on %s\nIPython: %s\nApp: %s%s\nInstance: %s\n' % (
         sys.version,
         sys.platform,
+        IPython.__version__,
         app.import_name,
         app.debug and ' [debug]' or '',
         app.instance_path,
