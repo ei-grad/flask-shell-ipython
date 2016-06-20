682bc56c562839b737ae5ea3f6528d1b2bf7f1ff
Andrew Grigorev <andrew@ei-grad.ru>
2016-06-20 19:01:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 setup(
-    name="Flask Shell IPython",
+    name="flask-shell-ipython",
+    author="Andrew Grigorev",
+    author_email="andrew@ei-grad.ru",
+    description="Replace default `flask shell` command by similar command running IPython.",
+    url="http://github.com/ei-grad/flask-shell-ipython",
     version="0.1",
     py_modules=['flask_shell_ipython'],
     install_requires=[
