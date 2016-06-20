816de9075a4860195b9cb07bd0faa03269ee3c02
Andrew Grigorev <andrew@ei-grad.ru>
2016-06-20 18:47:53 +0300

    Add install_requires to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ setup(
     name="Flask Shell IPython",
     version="0.1",
     py_modules=['flask_shell_ipython'],
+    install_requires=[
+        'flask>=0.11',
+        'click',
+        'IPython',
+    ],
     entry_points={
         'flask.commands': [
             'shell=flask_shell_ipython:shell',
