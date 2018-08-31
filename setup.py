711e90c8b952a7c87b40ac4973600542e712c559
Andrew Grigorev <andrew@ei-grad.ru>
2018-08-31 10:33:14 +0300

    0.3.1

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ setup(
     author="Andrew Grigorev",
     author_email="andrew@ei-grad.ru",
     description="Replace default `flask shell` command by similar command running IPython.",
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
     url="http://github.com/ei-grad/flask-shell-ipython",
-    version="0.2.2",
+    version="0.3.1",
     py_modules=['flask_shell_ipython'],
     install_requires=[
         'flask>=0.11',
@@ -17,4 +19,14 @@ setup(
             'shell=flask_shell_ipython:shell',
         ],
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Framework :: Flask',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ]
 )
